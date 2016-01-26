fa518fb85a0461c59d8cec0e7e9fa80ad71d7094
Jiapei Liang <liang98@purdue.edu>
2016-01-25 19:36:17 -0500

    Fix the undefined variable bug

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ var middleware = {
 		next();
 	},
 	logger: function (req, res, next) {
-		console.log(date + ' Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
 		next();
 	}
 };
